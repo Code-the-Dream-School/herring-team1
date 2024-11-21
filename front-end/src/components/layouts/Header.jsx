079d96c4bbd85ed3c6c7042c626b5931576908c6
mariyador <aniknorod@gmail.com>
2024-11-21 14:41:41 -0500

    add missing symbol

--- front-end/src/components/layouts/Header.jsx
+++ front-end/src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <nav className="flex m-4 justify-end space-x-4">
       <a href="/">Home</a>
       <br />
-      <a href="#mission">Our Mission</a>
+      <a href="/#mission">Our Mission</a>
       <br />
       <a href="/search">Find Oppotunities</a> <br />
       <a href="/register">Register</a> <br />
