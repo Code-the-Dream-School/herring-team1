e98317786c1339486b84fb94a712b12fda7dd05e
verafes <verafes@gmail.com>
2024-11-22 19:10:23 -0800

    add styling and links to footer

--- front-end/src/components/layouts/Footer.jsx
+++ front-end/src/components/layouts/Footer.jsx
@@ -1,13 +1,55 @@
 import { Link } from 'react-router-dom';
 
 function Footer() {
+  const isHomePage = window.location.pathname === '/';
   return (
     <footer className="bg-purple text-white text-center py-4 mt-auto">
-      <p>&copy; CareConnect</p>
-      <p>Designed and developed by Herring Team 1 for Code the Dream</p>
-      <Link to="/team" className="hover:text-white hover:underline">
-        The Team
-      </Link>
+      <div className="container text-small mx-auto px-4">
+        <div className="flex justify-center space-x-6 mb-4">
+          <a href="#nav" className="hover:underline text-white">
+            <i className="fas fa-chevron-up" />
+          </a>
+          <Link to="/" className="hover:underline text-white">
+            Home
+          </Link>
+          {isHomePage ? (
+            <a href="#mission" className="hover:underline text-white">
+              Our Mission
+            </a>
+          ) : (
+            <Link to="/#mission" className="hover:underline text-white">
+              Our Mission
+            </Link>
+          )}
+          <Link to="/search" className="hover:underline text-white">
+            Find Opportunities
+          </Link>
+          <Link to="/register" className="hover:underline text-white">
+            Register
+          </Link>
+          <Link to="/login" className="hover:underline text-white">
+            Login
+          </Link>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-4 text-sm space-y-4 md:space-y-0 md:space-x-8">
+          <div className="flex items-center space-x-8">
+            <p>All rights reserved &copy; CareConnect {new Date().getFullYear()} </p>
+            <div>
+              <a href="/privacy" className="hover:underline">
+                Privacy Policy
+              </a>{' '}
+              <span className="mx-2">|</span>
+              <a href="/legal" className="hover:underline">
+                Legal Info
+              </a>
+            </div>
+            <Link to="/team" className="hover:text-white hover:underline">
+              <i className="fas fa-users mr-2" />
+              The Team
+            </Link>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
