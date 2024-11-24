36e1cb659df1c1507258489eef7468dde4f2cdc5
mariyador <aniknorod@gmail.com>
2024-11-24 14:30:47 -0500

    add VolunteerProfileEditForm

--- front-end/src/components/pages/volunteer/VolunteerProfile.jsx
+++ front-end/src/components/pages/volunteer/VolunteerProfile.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { states } from '../../../data/states';
+import VolunteerProfileEditForm from './VolunteerProfileEditForm.jsx';
 
 function VolunteerProfile() {
   const [isEditing, setIsEditing] = useState(false);
@@ -39,124 +39,28 @@ function VolunteerProfile() {
   };
 
   const handleDelete = () => {
-    console.log('Delete your profile.');
+    console.info('Delete your profile.');
   };
   return (
     <div className="p-14">
-      <form>
-        <div className="flex space-x-4 mb-4">
-          {isEditing ? (
-            <>
-              <div className="w-1/2">
-                <label className="block text-sm font-bold">First Name</label>
-                <input
-                  type="text"
-                  name="firstName"
-                  value={formData.firstName}
-                  onChange={handleInputChange}
-                  className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300"
-                />
-              </div>
-              <div className="w-1/2">
-                <label className="block text-sm font-bold">Last Name</label>
-                <input
-                  type="text"
-                  name="lastName"
-                  value={formData.lastName}
-                  onChange={handleInputChange}
-                  className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300"
-                />
-              </div>
-            </>
-          ) : (
-            <div>
-              <label className="block text-sm font-bold">Name</label>
-              <p>
-                {formData.firstName} {formData.lastName}
-              </p>
-            </div>
-          )}
+      {isEditing ? (
+        <VolunteerProfileEditForm formData={formData} handleInputChange={handleInputChange} />
+      ) : (
+        <div>
+          <label className="block text-sm font-bold">Name</label>
+          <p>
+            {formData.firstName} {formData.lastName}
+          </p>
+          <label className="block text-sm font-bold">Address</label>
+          <p>
+            {formData.address}, {formData.city}, {formData.state}, {formData.zipCode}{' '}
+          </p>
+          <label className="block text-sm font-bold">About</label>
+          <p>{formData.about}</p>
+          <label className="block text-sm font-bold">Skills</label>
+          <p>{formData.skills}</p>
         </div>
-
-        <div className="mb-4">
-          {isEditing ? (
-            <div>
-              <label className="block text-sm font-bold">Address</label>
-              <input
-                type="text"
-                name="address"
-                value={formData.address}
-                onChange={handleInputChange}
-                className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300 mb-4"
-              />
-              <label className="block text-sm font-bold">City</label>
-              <input
-                type="text"
-                name="city"
-                value={formData.city}
-                onChange={handleInputChange}
-                className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300  mb-4"
-              />
-              <div className="flex space-x-4 mb-4">
-                <div className="w-1/2">
-                  <label className="block text-sm font-bold">State</label>
-                  <select
-                    name="state"
-                    value={formData.state}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300"
-                  >
-                    {states.map((state) => (
-                      <option key={state} value={state}>
-                        {state}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-                <div className="w-1/2">
-                  <label className="block text-sm font-bold">Zip Code</label>
-                  <input
-                    type="text"
-                    name="zipCode"
-                    value={formData.zipCode}
-                    onChange={handleInputChange}
-                    className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300"
-                  />
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div>
-              <label className="block text-sm font-bold">Address</label>
-              <p>
-                {formData.address}, {formData.city}, {formData.state}, {formData.zipCode}{' '}
-              </p>
-            </div>
-          )}
-        </div>
-
-        {Object.keys(formData)
-          .filter(
-            (field) =>
-              !['firstName', 'lastName', 'address', 'city', 'state', 'zipCode', 'phone', 'email'].includes(field)
-          )
-          .map((field) => (
-            <div key={field} className="mb-4">
-              <label className="block text-sm font-bold capitalize">{field.replace(/([A-Z])/g, ' $1')}</label>
-              {isEditing ? (
-                <textarea
-                  type="text"
-                  name={field}
-                  value={formData[field]}
-                  onChange={handleInputChange}
-                  className="border border-gray-300 rounded px-2 py-1 w-full shadow-md shadow-gray-300"
-                />
-              ) : (
-                <p>{formData[field]}</p>
-              )}
-            </div>
-          ))}
-      </form>
+      )}
       <div className="mt-6 flex space-x-4">
         {isEditing ? (
           <>
