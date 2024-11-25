e4bead381d859024fbd47187428e6ea665d10809
mariyador <aniknorod@gmail.com>
2024-11-24 20:16:38 -0500

    add Icons + responsive

--- front-end/src/components/pages/cards/OrganizationCard.jsx
+++ front-end/src/components/pages/cards/OrganizationCard.jsx
@@ -1,4 +1,5 @@
 import logoExample from '../../assets/images_default/logo_example.png';
+import { FaHeart, FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaGlobe } from 'react-icons/fa';
 
 function OrganizationCard() {
   const organizationLogo = null;
@@ -17,28 +18,42 @@ function OrganizationCard() {
 
   return (
     <div className="p-4">
-      <div className="flex flex-col bg-white border shadow-lg rounded-lg w-full p-4 mr-4">
-        <div className="flex items-center justify-between">
-          <div className="w-1/4 ">
-            <img src={formData.logo} alt={`${formData.name} logo`} />
+      <div className="flex flex-col bg-white border shadow-lg rounded-lg w-full p-4">
+        {/* First line */}
+        <div className="flex items-center justify-between lg:ml-10 mt-5">
+          <div className="lg:w-1/6 xs:w-1/3 md:w-1/4">
+            <img src={formData.logo} alt={`${formData.name} logo`} className="w-full h-full object-contain" />
           </div>
-          <h3 className="font-bold text-lg flex-1 text-center">{formData.name}</h3>
-          <p>Icon</p>
+          <h1 className="font-bold text-lg flex-1 text-center">{formData.name}</h1>
+          <FaHeart className="text-2xl text-red-500 lg:mr-10" />
         </div>
 
-        <div className="mt-4 text-left">
-          <p>
-            {formData.address}, {formData.city}, {formData.state} {formData.zipCode}
-          </p>
-          <p>
+        {/* Info about organization */}
+        <div className="text-left lg:ml-10 mt-8">
+          {/* Address */}
+          <div className="flex items-center mb-2">
+            <FaMapMarkerAlt className="mr-2" />
+            <p>
+              {formData.address}, {formData.city}, {formData.state} {formData.zipCode}
+            </p>
+          </div>
+          {/* Email */}
+          <div className="flex items-center mb-2">
+            <FaEnvelope className="mr-2" />
             <a href={`mailto:${formData.email}`}>{formData.email}</a>
-          </p>
-          <p>
+          </div>
+          {/* Website */}
+          <div className="flex items-center mb-2">
+            <FaGlobe className="mr-2" />
             <a href={formData.website} target="_blank" rel="noopener noreferrer">
               {formData.website}
             </a>
-          </p>
-          <p>Phone: {formData.phone}</p>
+          </div>
+          {/* Phone */}
+          <div className="flex items-center mb-2">
+            <FaPhoneAlt className="mr-2" />
+            <p>{formData.phone}</p>
+          </div>
         </div>
       </div>
     </div>
