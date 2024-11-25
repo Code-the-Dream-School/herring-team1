1a7f79c49c6d972e2cd43cfcf9445967fa3577ea
Oxana Michkasova <oxana.mitch@gmail.com>
2024-11-25 13:39:22 -0500

    Correct styling

--- front-end/src/components/layouts/Header.jsx
+++ front-end/src/components/layouts/Header.jsx
@@ -10,7 +10,6 @@ export default function Header() {
   const logoutUser = () => {
     localStorage.clear();
     logout();
-    setIsMenuOpen(false);
     navigate('/login');
   };
 
@@ -36,7 +35,7 @@ export default function Header() {
         <span className="block w-6 h-1 bg-gray-800" />
       </button>
 
-      <div className="flex space-x-6">
+      <div className="hidden md:flex space-x-6">
         <a href="/#mission" className="text-gray-800 hover:text-purple font-bold">
           Our Mission
         </a>
@@ -53,17 +52,21 @@ export default function Header() {
           <>
             <NavLink
               to="/dashboard"
-              className={({ isActive }) =>
-                `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
-              }
+              // className={({ isActive }) =>
+              //   `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
+              // }
             >
-              My Dashboard
+              <img
+                src="/src/assets/profile-user.png"
+                alt="Logo Connect Care"
+                className="h-6 w-6 transform hover:scale-110"
+              />
             </NavLink>
             <NavLink
               to="/auth/login"
               onClick={logoutUser}
               className={({ isActive }) =>
-                `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
+                `rounded-md w-[110px] h-[30px]  text-white flex items-center justify-center  font-bold  bg-light_grey ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
               }
             >
               Logout
@@ -73,7 +76,7 @@ export default function Header() {
           <NavLink
             to="/auth/login"
             className={({ isActive }) =>
-              `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
+              `w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey rounded-md'}`
             }
           >
             Login
@@ -83,7 +86,7 @@ export default function Header() {
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <div className="fixed inset-x-0 top-0 bg-white z-20 flex flex-col items-center pt-6 pb-10 shadow-lg rounded-b-lg w-full max-h-80">
+        <div className="fixed inset-x-0 top-0 bg-white z-20 flex flex-col items-center pt-6 pb-10 shadow-lg rounded-b-lg w-full max-h-120">
           <button className="absolute top-4 right-4 focus:outline-none" onClick={toggleMenu}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -127,19 +130,18 @@ export default function Header() {
             </NavLink>
             {user ? (
               <>
-                <NavLink
-                  to="/dashboard"
-                  className={({ isActive }) =>
-                    `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
-                  }
-                >
-                  My Dashboard
+                <NavLink to="/dashboard" className="flex justify-center items-center">
+                  <img
+                    src="/src/assets/profile-user.png"
+                    alt="Logo Connect Care"
+                    className="h-6 w-6 transform hover:scale-110"
+                  />
                 </NavLink>
                 <NavLink
                   to="/auth/login"
                   onClick={logoutUser}
                   className={({ isActive }) =>
-                    `px-4 py-2 rounded-md font-medium ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey'}`
+                    `rounded-md w-[110px] h-[30px]  text-white flex items-center justify-center  font-bold  bg-light_grey ${isActive ? 'w-[110px] h-[30px] text-white bg-purple hover:bg-gray-400 font-bold flex items-center justify-center rounded-md' : 'text-gray-700 hover:bg-light_grey rounded-md'}`
                   }
                 >
                   Logout
