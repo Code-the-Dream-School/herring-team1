d1be4a386e6304ac4b3e5bce80cd002951502272
mariyador <aniknorod@gmail.com>
2024-11-25 14:25:17 -0500

    Get one volunteer

--- back-end/app/controllers/volunteers_controller.rb
+++ back-end/app/controllers/volunteers_controller.rb
@@ -1,6 +1,22 @@
 class VolunteersController < ApplicationController
+    before_action :find_volunteer, only: [:show]
+  
+    # GET all volunteers
     def index
-        @volunteers = Volunteer.all
-        render json: @volunteers
+      @volunteers = Volunteer.all
+      render json: @volunteers
     end
-end
+  
+    # GET one volunteer
+    def show
+      render json: @volunteer
+    end
+  
+    private
+  
+    def find_volunteer
+      @volunteer = Volunteer.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Volunteer not found" }, status: :not_found
+    end
+  end
