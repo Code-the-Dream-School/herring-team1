d430dd28d812072a0a1457487bcecc590262a2be
mariyador <aniknorod@gmail.com>
2024-11-25 14:55:06 -0500

    update volunteer

--- back-end/app/controllers/volunteers_controller.rb
+++ back-end/app/controllers/volunteers_controller.rb
@@ -1,5 +1,5 @@
 class VolunteersController < ApplicationController
-    before_action :find_volunteer, only: [:show]
+    before_action :find_volunteer, only: [:show, :update]
   
     # GET all volunteers
     def index
@@ -12,11 +12,27 @@ class VolunteersController < ApplicationController
       render json: @volunteer
     end
   
+
+    # UPDATE volunteer
+    def update
+        if @volunteer.update(volunteer_params)
+        render json: { message: "Volunteer updated successfully", volunteer: @volunteer }, status: :ok
+        else
+        render json: { error: "Failed to update volunteer", details: @volunteer.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
     private
   
+    # Find volunteer by ID
     def find_volunteer
       @volunteer = Volunteer.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       render json: { error: "Volunteer not found" }, status: :not_found
+    end
+
+    # Params for update
+    def volunteer_params
+        params.require(:volunteer).permit(:first_name, :last_name, :address, :city, :state, :zip_code, :phone, :email, :about, :services, :profile_img)
     end
   end
--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
                 registrations: 'auth/registrations'
               }
 
-  resources :volunteers, only: [:index, :show]
+  resources :volunteers, only: [:index, :show, :update]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
