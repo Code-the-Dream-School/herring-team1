28bc6bc7706338e252b26322451652c9927d589f
Iryna Kolhanova <iryna.kolhanova@gmail.com>
2024-11-25 17:04:39 -0500

    fix cors

--- back-end/config/application.rb
+++ back-end/config/application.rb
@@ -42,16 +42,5 @@ module RestRails
     config.api_only = true
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://127.0.0.1:5173', 'http://localhost:5173'
-    
-        resource '*',
-          headers: :any,
-          methods: [:get, :post, :put, :patch, :delete, :options, :head],
-          credentials: true
-      end
-    end
   end
 end
