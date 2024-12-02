2f8ccc4e178314ffb77a1e42b39046ae97e8f6ca
verafes <verafes@gmail.com>
2024-12-01 19:31:05 -0800

    fix syntax

--- back-end/db/migrate/20241202025445_add_email_to_organizations.rb
+++ back-end/db/migrate/20241202025445_add_email_to_organizations.rb
@@ -1,3 +1,4 @@
+# This migration adds an email column to the organizations table.
 class AddEmailToOrganizations < ActiveRecord::Migration[7.1]
   def change
     add_column :organizations, :email, :string
