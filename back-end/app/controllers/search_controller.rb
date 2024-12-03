1f26770a36094132d70441a552b6b3a1c53c19ee
OksanaCTD <fetter.oksana@gmail.com>
2024-12-03 10:08:59 -0500

    CR-23-BE-Search-OF

---
+++ back-end/app/controllers/search_controller.rb
@@ -0,0 +1,47 @@
+class SearchController < ApplicationController
+  # check if record not found
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::StatementInvalid, with: :invalid_query
+
+  # search_by_zip_code using address model
+  def search_by_zip_code
+    zip_code = params[:zip_code]
+    # check if zip_code is blank
+    return render json: { error: 'zip_code is required' }, status: :bad_request if zip_code.blank?
+
+    @addresses = Address.where(zip_code: zip_code).includes(:organization)
+    @organizations = @addresses.map(&:organization).compact
+
+    render json: @organizations
+  end
+
+  # search_by_keyword using organization model
+  def search_by_keyword
+    keyword = params[:keyword]
+    # check if keyword is blank
+    return render json: { error: 'keyword is required' }, status: :bad_request if keyword.blank?
+
+    @organizations = Organization.where('name LIKE ? OR description LIKE ?', "%#{keyword}%", "%#{keyword}%")
+    render json: @organizations
+  end
+
+  # search_by_services using organization model
+  def search_by_services
+    service = params[:service]
+    # check if service is blank
+    return render json: { error: 'service is required' }, status: :bad_request if service.blank?
+
+    @organizations =Organization.where('services @> ?',"{#{service}}") 
+    render json: @organizations
+  end
+
+  private
+
+  def record_not_found
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def invalid_query
+    render json: { error: 'Invalid query' }, status: :bad_request
+  end
+end
--- back-end/config/routes.rb
+++ back-end/config/routes.rb
@@ -6,6 +6,15 @@ Rails.application.routes.draw do
               }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # search_by_zip_code using address model
+  get 'search/zip', to: 'search#search_by_zip_code'
+
+  # search by keyword using organization model
+  get 'search/keyword', to: 'search#search_by_keyword'
+
+  # search by services using organization model
+  get 'search/services', to: 'search#search_by_services'
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
