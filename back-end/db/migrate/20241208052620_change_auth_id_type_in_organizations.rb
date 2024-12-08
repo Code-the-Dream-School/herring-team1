97e369c6d49e4bb61f226d7246d199a087c4757f
verafes <verafes@gmail.com>
2024-12-07 22:20:24 -0800

    fix syntax

--- back-end/db/migrate/20241208052620_change_auth_id_type_in_organizations.rb
+++ back-end/db/migrate/20241208052620_change_auth_id_type_in_organizations.rb
@@ -1,4 +1,4 @@
-# Migration to change the data type of `auth_id` in the organizations table 
+# Migration to change the data type of `auth_id` in the organizations table
 class ChangeAuthIdTypeInOrganizations < ActiveRecord::Migration[7.1]
   def change
     change_column :organizations, :auth_id, :integer, null: false
