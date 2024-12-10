d0c1d37491fa47c42529664b50961bf2beb3f8f6
OksanaCTD <fetter.oksana@gmail.com>
2024-12-09 23:32:31 -0500

    modify storage

--- back-end/db/migrate/20241210012919_create_active_storage_tables.active_storage.rb
+++ back-end/db/migrate/20241210012919_create_active_storage_tables.active_storage.rb
@@ -1,7 +1,13 @@
-# This migration comes from active_storage (originally 20170806125915)
 class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
   def change
-    # Use Active Record's configured type for primary and foreign keys
+    create_blobs_table
+    create_attachments_table
+    create_variant_records_table
+  end
+
+  private
+
+  def create_blobs_table
     primary_key_type, foreign_key_type = primary_and_foreign_key_types
 
     create_table :active_storage_blobs, id: primary_key_type do |t|
@@ -21,6 +27,10 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
 
       t.index [:key], unique: true
     end
+  end
+
+  def create_attachments_table
+    primary_key_type, foreign_key_type = primary_and_foreign_key_types
 
     create_table :active_storage_attachments, id: primary_key_type do |t|
       t.string     :name,     null: false
@@ -36,6 +46,10 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
       t.index [:record_type, :record_id, :name, :blob_id], name: :index_active_storage_attachments_uniqueness, unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
+  end
+
+  def create_variant_records_table
+    primary_key_type, foreign_key_type = primary_and_foreign_key_types
 
     create_table :active_storage_variant_records, id: primary_key_type do |t|
       t.belongs_to :blob, null: false, index: false, type: foreign_key_type
@@ -45,8 +59,6 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
       t.foreign_key :active_storage_blobs, column: :blob_id
     end
   end
-
-  private
 
   def primary_and_foreign_key_types
     config = Rails.configuration.generators
