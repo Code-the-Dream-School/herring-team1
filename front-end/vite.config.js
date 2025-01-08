9d1e3b48d237d3eb72ef7ba196edc27c37e3ffb3
OksanaCTD <fetter.oksana@gmail.com>
2025-01-07 21:33:42 -0500

    remove duplicates

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -24,12 +24,6 @@ export default defineConfig({
       secure: false,
       rewrite: (path) => path.replace(/^\/organizations/, '/organizations'),
       },
-      '/volunteers': {
-        target: 'http://127.0.0.1:3000/',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/volunteers/, "/volunteers"),
-      },
       '/requests': {
         target: 'http://127.0.0.1:3000/',
         changeOrigin: true,
