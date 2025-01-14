a35e639f59986ba84e24814f65708762ad93fc1e
mariyador <aniknorod@gmail.com>
2025-01-13 22:01:42 -0500

    minor changes

--- front-end/src/components/pages/organization/InfoPage.jsx
+++ front-end/src/components/pages/organization/InfoPage.jsx
@@ -235,7 +235,7 @@ function InfoPage() {
                 ) : (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 24 24"
+                    viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
                     strokeWidth="2"
