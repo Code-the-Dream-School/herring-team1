43cd090ed5112d9ca893803bc22f9024ef0b64d9
John R. McGarvey <johnmcgarvey55@gmail.com>
2025-01-28 12:23:45 -0500

    another try to fix fog

--- back-end/config/initializers/carrierwave.rb
+++ back-end/config/initializers/carrierwave.rb
@@ -1,9 +1,10 @@
+module CarrierWave; module Storage; class Fog; end; end; end
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: 'Cloudinary',
     cloud_name: ENV.fetch('CLOUDINARY_CLOUD_NAME', nil),
     api_key: ENV.fetch('CLOUDINARY_API_KEY', nil),
-    api_secret: ENV.fetch('CLOUDINARY_API_SECRET', nil)
+    api_secret: ENV.fetch('CLOUDINARY_API_SECRET', nil)  
   }
   config.cache_storage = :file
 end
--- back-end/config/initializers/fix_fog.rb
+++
@@ -1,2 +0,0 @@
-# config/initializers/fix_fog.rb
-module CarrierWave; module Storage; class Fog; end; end; end
